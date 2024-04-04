e457e3770cea6a9a858db3970148f3945ad350ca
aube.lee <107289988+2aurore@users.noreply.github.com>
2024-04-04 11:33:56 +0900

    Title: 2 또는 3의 배수, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240404/2 또는 3의 배수/multiple-of-2-or-3.py
+++ 240404/2 또는 3의 배수/multiple-of-2-or-3.py
@@ -1,8 +1,8 @@
 n= int(input())
 
 result = ''
-for i in range(n):
-    if (i+1) % 2 == 0 or (i+1) % 3 == 0 :
+for i in range(1, n+1):
+    if (i) % 2 == 0 or (i) % 3 == 0 :
         result += '1'
     else: 
         result += '0'
