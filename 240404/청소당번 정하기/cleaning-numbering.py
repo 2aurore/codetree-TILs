4a7ca27b4ec1c4351ef5de6e40227ddf9ff9e5f1
aube.lee <107289988+2aurore@users.noreply.github.com>
2024-04-04 12:50:35 +0900

    Title: 청소당번 정하기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240404/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+room = 0
+floor = 0
+bath = 0
+
+for i in range(1, n):
+    if i % 2 == 0 and i % 3 != 0 and i % 12 != 0:
+        room += 1
+    elif i % 3 == 0 and i % 12 != 0:
+        floor += 1
+    elif i % 12 == 0:
+        bath += 1
+
+print(room, floor, bath)
