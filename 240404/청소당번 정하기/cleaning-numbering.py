5503c0164d060452b9de6bf4bc918d500264193b
aube.lee <107289988+2aurore@users.noreply.github.com>
2024-04-04 12:52:20 +0900

    Title: 청소당번 정하기, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 240404/청소당번 정하기/cleaning-numbering.py
+++ 240404/청소당번 정하기/cleaning-numbering.py
@@ -4,7 +4,7 @@ room = 0
 floor = 0
 bath = 0
 
-for i in range(1, n):
+for i in range(1, n+1):
     if i % 2 == 0 and not(i % 3 == 0) and not(i % 12 == 0):
         room += 1
     elif i % 3 == 0 and not(i % 12 == 0):
